b6eb0516255166597fee75f3ebdccdc860a4591d
dstefanelli <die.stefanelli@gmail.com>
2025-03-06 22:42:51 +0100

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt({
+    sitemap: false,
+    policy: [
+      {
+        userAgent: '*',
+        disallow: '/',
+      },
+    ],
+  }),],
   site: 'https://dstefanelli.dev/',
-  integrations: [
-    robotsTxt({
-      sitemap: false,
-      policy: [
-        {
-          userAgent: '*',
-          disallow: '/',
-        },
-      ],
-    }),
-  ],
   output: "static",
-  // vite: {
-  //   server: {
-  //     host: "0.0.0.0",
-  //     strictPort: true,
-  //     hmr: {
-  //       clientPort: 443,
-  //     },
-  //     allowedHosts: ["bug-today-silicon-errors.trycloudflare.com"],
-  //   },
-  // },
 })
