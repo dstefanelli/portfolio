a58040c9b94797dfee8bda02534a8ce9086737bb
dstefanelli <die.stefanelli@gmail.com>
2025-03-06 09:48:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<div align="center">
+
+<p></p>
+</div>
+
+<div align="center">
+
+![Astro Badge](https://img.shields.io/badge/Astro-FF3E00?logo=astro&logoColor=fff&style=flat)
+![Tailwind CSS Badge](https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=tailwindcss&logoColor=fff&style=flat)
+
+</div>
+
---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/',
+  vite: {
+    server: {
+      host: "0.0.0.0",
+      strictPort: true,
+      hmr: {
+        clientPort: 443,
+      },
+      allowedHosts: ["bug-today-silicon-errors.trycloudflare.com"],
+    },
+  },
+})
