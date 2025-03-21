ad3a4537f64db1f2ce4bf17bdb68efd20d218885
dstefanelli <die.stefanelli@gmail.com>
2025-03-21 19:03:49 +0100

    experience localization

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,28 +8,48 @@ export const showDefaultLang = false;
 
 export const ui = {
   en: {
-    "nav.experience": "Experience",
-    "nav.projects": "Projects",
-    "nav.about": "About me",
-    "nav.contact": "Contact",
+    "about.description":
+      "My name is Diego Stefanelli. I started working in frontend development 12 years ago. I am currently <strong>specialized in working with React and Vue technologies</strong>. I’ve worked on all kinds of projects, from candidate management applications to automotive, oil industry or services. I have experience with both GitLab and GitHub, and unit testing with Vitest and Jest. I’ve worked closely with design and product teams, and I’ve had the opportunity to collaborate with development teams in different countries.</br>Personally, I enjoy spending time outdoors, I travel whenever I can, and I love photography. I’m passionate about technology and always looking to learn something new. I also enjoy cycling and hiking. Motorcycles are another one of my favorite hobbies.",
+    "about.title": "About me",
+    "experience.job_title": "Frontend Developer",
+    "experience.job_one_date": "August 2019 - February 2025",
+    "experience.job_one_description":
+      "Frontend development for enterprise survey and action plan services, collaborating with a team to deliver solutions for global clients. Working with Vue and React, integrating REST APIs to optimize the user experience.",
+    "experience.job_two_date": "June 2017 - July 2019",
+    "experience.job_two_description":
+      "Contributed to the development of software for candidate and employee management for companies in Latin America. Development in .NET and Razor with Vanilla JavaScript and Bootstrap on the frontend.",
+    "experience.job_three_date": "November 2014 - February 2017",
+    "experience.job_three_description":
+      "Worked as part of a team within the Accenture consulting firm, developing applications for international clients in .NET environments, including platforms such as Sitecore.",
+    "footer.rights": "All rights reserved",
+    "footer.contact": "Contact",
     "hero.title": "Hi 👋🏼, I'm Diego!",
     "hero.badge": "Open to work",
     "hero.contact": "Contact me",
     "hero.description":
       "Passionate and proactive <strong>Frontend Developer</strong> with extensive experience in designing and developing web applications using modern technologies such as <strong>React, Vue, and TypeScript</strong>. I excel at creating user-friendly interfaces, optimizing performance, and ensuring seamless user experiences.</br>I have <strong>strong problem-solving abilities, effective communication skills, and adaptability</strong>, which allow me to thrive in fast-paced, agile environments. I enjoy working collaboratively, embracing new challenges, and continuously expanding my technical knowledge to stay up to date with the latest industry trends.",
-    "about.title": "About me",
-    "about.description":
-      "My name is Diego Stefanelli. I started working in frontend development 12 years ago. I am currently <strong>specialized in working with development teams at multinational companies using React and Vue technologies</strong>. I’ve worked on all kinds of projects, from candidate management applications to enterprise surveys. I have experience with both GitLab and GitHub, and unit testing with Vitest and Jest. I’ve worked closely with design and product teams, and I’ve had the opportunity to collaborate with development teams in different countries.</br>Personally, I enjoy spending time outdoors, I travel whenever I can, and I love photography. I’m passionate about technology and always looking to learn something new. I also enjoy cycling and hiking. Motorcycles are another one of my favorite hobbies.",
-    "theme.selector": "Choose the Theme",
-    "footer.rights": "All rights reserved",
-    "footer.contact": "Contact",
+    "nav.about": "About me",
+    "nav.contact": "Contact",
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
     "page.experience_title": "Work experience",
     "page.about_title": "About me",
+    "theme.selector": "Choose the Theme",
   },
   es: {
     "about.description":
       "Me llamo Diego Stefanelli. Empecé en el desarrollo frontend hace 12 años. Actualmente estoy <strong>especializado en trabajar con las tecnologías React y Vue</strong>.</br>He trabajado en proyectos de todo tipo, desde aplicaciones de gestión de candidatos hasta automoción, indústria petrolera o servicios. Con experiencia tanto en GitLab como GitHub, y test unitarios con Vitest y Jest.</br>Trabajo a la par con equipos de diseño y de producto, y he tenido la oportunidad de trabajar con equipos de desarrollo en diferentes países.</br>Personalmente me gusta pasar tiempo al aire libre, viajo siempre que puedo y me encanta la fotografía. Soy un apasionado de la tecnología y siempre estoy buscando aprender algo nuevo. También me gusta andar en bicicleta y hacer senderismo. Las motos son otro de mis pasatiempos favoritos.",
     "about.title": "Sobre mí",
+    "experience.job_title": "Desarrollador Frontend",
+    "experience.job_one_date": "Agosto 2019 - Febrero 2025",
+    "experience.job_one_description":
+      "Desarrollo frontend en el servicio de encuestas empresariales y planes de acción, colaborando con un equipo para ofrecer soluciones a clientes globales. Trabajo con Vue y React, integrando APIs REST para optimizar la experiencia del usuario.",
+    "experience.job_two_date": "Junio 2017 - Julio 2019",
+    "experience.job_two_description":
+      "Participaba en el desarrollo de software para la gestión de candidatos y empleados en empresas de Latinoamérica. Desarrollo en .NET y Razor con Vanilla JavaScript y Bootstrap en el frontend.",
+    "experience.job_three_date": "Noviembre 2014 - Febrero 2017",
+    "experience.job_three_description":
+      "Trabajaba en un equipo dentro de la consultora Accenture, desarrollando aplicaciones para clientes internacionales en entornos .NET, incluyendo plataformas como Sitecore.",
     "footer.rights": "Todos los derechos reservados",
     "footer.contact": "Contacto",
     "hero.badge": "Disponible para trabajar",
